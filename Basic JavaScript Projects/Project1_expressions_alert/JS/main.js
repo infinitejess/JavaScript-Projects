dc8646620e7ebc810d12f9a4b1b2a3be76c6529f
Jessica Fellows <jessicaxfellows@gmail.com>
2022-03-04 17:19:13 -0500

    JavaScript Assignment One

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,13 @@
+var A="This is a string";    //Assigning variable A
+window.alert(A);    //Creating a pop-up with A printed
+document.write(" \"What are you doing here?\" I asked.<br>");   //Printing a quote with "" in it and line break
+document.write("I brought the " +" pizza.<br>");   //Printing a reply with a line break
+var X="Putting";   // Assigning variable
+var Y=" Strings";   //Assigning a varibale
+var Z=" Together.<br>";   //Assigning a varibale
+document.write(X+Y+Z);    //Concatenating the three variables
+var str = new String(X+Y+Z);  //Declaring new string variable
+document.write(str.fontcolor("red"));   //Making it red
+var Family = "The Innocents", Dad = "Jp", Mom = "Jess"; Dog = "Nami";   //Assigning multiple variables 
+document.write(Dog);   //Printing Dog variable
+document.write(5*10);   //Printing an expression
